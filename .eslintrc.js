326ddafebb026b3dce8636470b9f4338b1604911
Komal Ali <komalali@uw.edu>
2018-04-24 14:25:08 -0700

    add bar chart as example

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "extends": "airbnb-base"
+  "extends": "airbnb-base",
+  "rules": {
+    "no-plusplus": 0,
+    "no-mixed-operators": 0,
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,83 @@
 import * as d3 from 'd3';
 import './index.css';
 
+// Make this a function so it can be easily reused when updating the chart.
+function generateRandomDataset(numberOfDataPoints) {
+  const dataPoints = [];
+  for (let i = 0; i < numberOfDataPoints; i++) {
+    const randomNumber = Math.floor(Math.random() * 21) + 5;
+    dataPoints.push(randomNumber);
+  }
+  return dataPoints;
+}
+
+// Properties for our chart
+const properties = {
+  width: 800,
+  height: 600,
+  barPadding: 1,
+  data: generateRandomDataset(20),
+  labelOffset: 14,
+};
+
 d3.select('.chart-title')
   .append('h4')
-  .text('This is the title of this chart');
+  .text('This is the title of this chart')
+  .style('color', 'red');
+
+const svg = d3.select('.chart')
+  .append('svg')
+  .attr('height', properties.height)
+  .attr('width', properties.width);
+
+const xScale = d3.scaleBand() // instead of using scale.ordinal and then rangeBand, d3v4 has a scaleBand()
+  .rangeRound([0, properties.width])
+  .domain(d3.range(properties.data.length));
+
+const yScale = d3.scaleLinear() // d3v4 syntax
+  .domain([0, d3.max(properties.data)])
+  .range([0, properties.height]);
+
+svg.selectAll('rect')
+  .data(properties.data)
+  .enter()
+  .append('rect')
+  .attr('x', (datum, index) => xScale(index))
+  .attr('y', datum => properties.height - yScale(datum)) // Arrow functions from ES6
+  .attr('width', xScale.bandwidth())
+  .attr('height', datum => yScale(datum))
+  .attr('fill', datum => `rgb(0, 0, ${datum * 10})`); // Template-literals from ES6
+
+svg.selectAll('text')
+  .data(properties.data)
+  .enter()
+  .append('text')
+  .attr('class', 'label')
+  .text(datum => datum)
+  .attr('x', (datum, index) => xScale(index) + xScale.bandwidth() / 2)
+  .attr('y', datum => properties.height - yScale(datum) + properties.labelOffset);
+
+function updateChart() {
+  const data = generateRandomDataset(20);
+  const { height, labelOffset } = properties; // object-destructuring from ES6
+
+  svg.selectAll('rect')
+    .data(data)
+    .transition()
+    .duration(500)
+    .attr('y', datum => height - yScale(datum))
+    .attr('height', datum => yScale(datum))
+    .attr('fill', datum => `rgb(0, 0, ${datum * 10})`);
+
+  svg.selectAll('text')
+    .data(data)
+    .transition()
+    .duration(500)
+    .text(datum => datum)
+    .attr('x', (datum, index) => xScale(index) + xScale.bandwidth() / 2)
+    .attr('y', datum => height - yScale(datum) + labelOffset);
+}
+
+d3.select('.button')
+  .on('click', updateChart);
+
